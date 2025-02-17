171751b8955297f6fc037b34b89a28b789e65f8d
Marco <marcocm970@gmail.com>
2025-02-17 11:59:02 +0100

    Update Bloque6_TestUnitarios.java

--- Bloque6_TestUnitarios.java
+++ Bloque6_TestUnitarios.java
@@ -8,7 +8,7 @@ class Calculadora {
     }
 }
 
-public class Man {
+public class Main {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
         System.out.println(calculadora.sumar(2, 3)); // Sin haber escrito un test primero
